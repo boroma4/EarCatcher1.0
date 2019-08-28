c9a9df71f6e85a0ea2e0d3d09b4e6a1b6efde630
Bohdan Romashchenko <bohdan.romashchenko@gmail.com>
2019-08-28 13:07:57 +0300

    Initial commit

---
+++ DrumsIno/Loud_Slave/Loud_Slave.ino
@@ -0,0 +1,165 @@
+#include <SPI.h>
+#include <SD.h>
+#include <pcmConfig.h>
+#include <pcmRF.h>
+#include <TMRpcm.h>
+#include <Wire.h>
+#define SD_ChipSelectPin 4
+#define MAX_SONGS 4
+
+
+bool trackstart = false;
+bool Setup = false;
+bool SD_Setup = false;
+bool isPaused = false;
+TMRpcm music;
+char *songs[]  = { "drums.wav", "drums2.wav", "drums3.wav", "drums4.wav", "drums5.wav"};
+byte volumeCount = 3;
+byte songCount = 0;
+byte command = 0;
+const int Reset PROGMEM = 8;
+
+void setup()
+{
+  Serial.begin(9600);
+  music.speakerPin = 9;
+
+  Wire.begin(1);
+  Wire.onReceive(receiveEvent);
+  if (!SD.begin(SD_ChipSelectPin))
+  {
+    Serial.println(F("SD fail"));
+    SD_Setup = false;
+  }
+  else
+  {
+    Serial.println(F("SD sucess"));
+    SD_Setup = true;
+  }
+  music.setVolume(0);
+  music.quality(1);
+  music.loop(1);
+
+}
+void receiveEvent(int bytes)
+{
+  command  = Wire.read();
+  Serial.print("I recieved ");// read one character from the I2C
+  Serial.println(command);
+  if (command == 6)
+  {
+    music.stopPlayback();
+    volumeCount = 3;
+    songCount = 0;
+    trackstart = false;
+    delay(100);
+    return;
+  }
+  if (!trackstart && !isPaused)
+  {
+    trackstart = true;
+    Serial.print("Time to start... ");
+    music.play(songs[songCount]);
+  }
+  if (command == 7 or command == 8)
+  {
+    if (command == 7)
+    {
+      if (volumeCount < 4)
+      {
+        volumeCount++;
+      }
+    }
+    if (command == 8)
+    {
+      if (volumeCount > 1)
+      {
+        volumeCount--;
+      }
+    }
+    return;
+  }
+
+  if (command == 9)
+  {
+    if (trackstart)
+    {
+      music.pause();
+    }
+    if (!isPaused)
+    {
+      isPaused = true;
+    }
+    else
+    {
+      isPaused = false;
+    }
+    return;
+  }
+  if (command == 4 or command == 5)
+  {
+    music.disable();
+    delay(500);
+    if (command == 4)
+    {
+      songUp();
+    }
+    if (command == 5)
+    {
+      songDown();
+    }
+    return;
+  }
+  if (trackstart)
+  {
+    if (command == 0)
+    {
+      mute();
+      Serial.println(F("Muted..."));
+    }
+    if (command == 2 or command == 1 )
+    {
+      music.setVolume(volumeCount);
+      Serial.println(F("Unpaused..."));
+    }
+  }
+}
+
+void loop()
+{
+
+  delay(100);
+}
+
+void mute ()
+{
+  music.setVolume(1);
+ 
+
+}
+
+void songUp()
+{
+  if (songCount < MAX_SONGS)
+  {
+    songCount++;
+    trackstart = false;
+  }
+  else
+  {
+    songCount = 0;
+    trackstart = false;
+  }
+}
+void songDown()
+{
+  if (songCount > 0) {
+    songCount--;
+    trackstart = false;
+  }
+  else
+  {
+    songCount = MAX_SONGS;
+    trackstart = false;
+  }
+}
---
+++ LoudSpeaker_Master/LoudSpeaker_Master.ino
@@ -0,0 +1,418 @@
+#include <EEPROM.h>
+
+#include <boarddefs.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
+
+#include <LiquidCrystal_PCF8574.h>
+#include <EarCatcher.h>
+#include <Wire.h>
+
+
+#define MAX_SONGS 5
+#define VOLTAGE_LVL 0.15
+
+byte command1 = 0;
+byte command2 = 0;
+byte command3 = 0;
+byte command4 = 0;
+
+byte trackNum = 1;
+byte mute = 0;
+int trackVol = 0;
+int lastVol = 4;
+int blowingCount = 0;
+int highscoreCount = 0;
+
+
+float voltage1 = 0;
+float voltage2 = 0;
+float voltage3 = 0;
+float voltage4 = 0;
+int highscore = 0;
+
+bool signalRec = false;
+bool bLcdSetup = false;
+bool cheatMode = false;
+bool begincom = false;
+bool wasSwitched = false;
+bool isMuted1 = false;
+bool isPaused = false;
+bool display_cycleOne = false;
+bool display_cycleTwo = false;
+
+
+const int RECV_PIN = 5;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+
+LiquidCrystal_PCF8574 lcd(0x3F); // defining display through I2C
+LiquidCrystal_PCF8574 lcd2(0x27); // defining display through I2C
+ReadValues volts ; // object of ReadValues class EarCatcher lib
+
+void setup()
+{
+  //  Start the I2C Bus as Master
+  Wire.begin();
+  Serial.begin(9600); //Serial Com for debugging
+  lcd.init();                      // Инициализация дисплея
+  lcd.setBacklight(255);
+  lcd.begin(16, 2);
+  lcd2.init();                      // Инициализация дисплея
+  lcd2.setBacklight(255);
+  lcd2.begin(16, 2);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+
+  EEPROM.get (0,highscore);
+  lcd2.clear();
+  lcd2.setCursor(0, 0);
+  lcd2.print("HIGHSCORE:");
+  lcd2.setCursor(4, 1); // Установка курсора в начало второй строки
+  lcd2.print(highscore);
+  if (highscore != 1) {
+        lcd2.print(" points");
+      }
+      else {
+        lcd2.print(" point");
+      }
+  // Подключение подсветки
+}
+
+void(* resetFunc) (void) = 0;
+
+void loop()
+{
+  Wire.beginTransmission(1); // transmit to device #9
+  Wire.write(command1);
+  Wire.endTransmission(false);
+
+  Wire.beginTransmission(2); // transmit to device #9
+  Wire.write(command2);
+  Wire.endTransmission(false);
+
+  Wire.beginTransmission(3); // transmit to device #9
+  Wire.write(command3);
+  Wire.endTransmission(false);
+
+  Wire.beginTransmission(4); // transmit to device #9
+  Wire.write(command4);
+  Wire.endTransmission();
+  /*
+     SEND COMMAND TO OTHER ARDUINOS HERE
+
+  */
+  delay(100);
+
+  if (command1 == 6) // resetting master arduino
+  {
+    delay(100);
+    resetFunc();
+  }
+  delay(100);
+
+  voltage1 =  volts.readVolt(A4); // returns voltage
+  voltage2 = volts.readVolt(A15);
+  voltage3 =  volts.readVolt(A6); // returns voltage
+  voltage4 = volts.readVolt(A9);
+
+  if (!bLcdSetup) // Update display info
+  {
+    display_update(); // ONLY SLAVE ONE UPDATES DISPLAY WITH VOLUME AND TRACK
+  }
+  if (cheatMode) // Cheats
+  {
+    voltage1 = 999;
+    voltage2 = 999;
+    voltage3 = 999;
+    voltage4 = 999;
+  }
+
+  /*
+     HIGHSCORE CHECK
+  */
+
+  if (!isPaused and !cheatMode and (powerCheck(voltage1, VOLTAGE_LVL) or powerCheck(voltage2, VOLTAGE_LVL) or powerCheck(voltage3, VOLTAGE_LVL) or powerCheck(voltage4, VOLTAGE_LVL) ))
+  {
+    blowingCount++;
+  }
+  else
+  {
+    if (blowingCount > highscoreCount)
+    {
+      highscoreCount = blowingCount;
+      if (highscoreCount > 2) {
+        highscore = highscoreCount / 2;
+      }
+      lcd2.clear();
+      lcd2.setCursor(0, 0);
+      lcd2.print("HIGHSCORE:");             // Установка курсора в начало второй строки
+
+      lcd2.setCursor(4, 1);
+      lcd2.print(highscore);
+      if (highscore != 1) {
+        lcd2.print(" points");
+      }
+      else {
+        lcd2.print(" point");
+      }
+      EEPROM.put (0, highscore);
+    }
+    blowingCount = 0;
+  }
+
+  // COMMON COMMANDS FOR ALL SLAVES
+
+  if (irrecv.decode(&results)) {
+
+    switch (results.value)
+    {
+      case 0xFF02FD:
+
+        Serial.println ("next track");
+        command1 = 4;
+        command2 = 4;
+        command3 = 4;
+        command4 = 4;
+        bLcdSetup = false;
+        songUp();
+        irrecv.resume();
+        return;
+
+      case 0xFF22DD:
+
+        Serial.println ("previous track");
+        command1 = 5;
+        command2 = 5;
+        command3 = 5;
+        command4 = 5;
+        bLcdSetup = false;
+        songDown();
+        irrecv.resume();
+        return;
+
+      // reset highscore
+      case 0xFF5AA5 :
+
+        EEPROM.put(0, 0);
+        highscore = 0;
+        highscoreCount = 0;
+        lcd2.clear();
+        lcd2.setCursor(0, 0);
+        lcd2.print("HIGHSCORE:");             // Установка курсора в начало второй строки
+        lcd2.setCursor(4, 1);
+        lcd2.print(highscore);
+        lcd2.print(" points");
+        irrecv.resume();
+        return;
+
+      case 0xFF42BD:
+
+        Serial.println ("Cheat mode activated!");
+        cheatMode = true;
+        irrecv.resume();
+        return;
+
+      case  0xFF4AB5:
+
+        Serial.println ("Cheat mode disabled!");
+        cheatMode = false;
+        irrecv.resume();
+        return;
+
+      case 0xFFA857:
+
+        Serial.println ("Volume up!");
+        command1 = 7;
+        command2 = 7;
+        command3 = 7;
+        command4 = 7;
+
+        if (lastVol < 5)
+        {
+          lastVol++;
+        }
+        bLcdSetup = false;
+        irrecv.resume();
+        return;
+
+      case 0xFFE01F :
+
+        Serial.println ("Volume down!");
+        command1 = 8;
+        command2 = 8;
+        command3 = 8;
+        command4 = 8;
+        if (lastVol > 1)
+        {
+          lastVol--;
+        }
+        bLcdSetup = false;
+        irrecv.resume();
+
+        return;
+
+      // PAUSE
+      case 0xFFC23D :
+        command1 = 9;
+        command2 = 9;
+        command3 = 9;
+        command4 = 9;
+        bLcdSetup = false;
+
+        if (!isPaused)
+        {
+          isPaused = true;
+        }
+        else
+        {
+          isPaused = false;
+        }
+        irrecv.resume();
+
+        return;
+
+
+      case 0xFF52AD : // reset function
+
+        command1 = 6;
+        command2 = 6;
+        command3 = 6;
+        command4 = 6;
+        irrecv.resume();
+        return;
+
+      default :
+        irrecv.resume();
+
+    }
+
+  }
+  // SEPARATE COMMANDS FOR SLAVES
+
+  /*
+     FAN N1 ( pin A2)
+  */
+
+  if (!(powerCheck(voltage1, VOLTAGE_LVL))) // if u need to mute track n1
+  {
+    command1 = 0;
+    if (!display_cycleOne)
+    {
+      bLcdSetup = false;
+      display_cycleOne = true;
+    }
+    isMuted1 = true;
+    display_cycleTwo = false;
+  }
+  if (powerCheck(voltage1, VOLTAGE_LVL) ) // if u need to unmute track 1
+  {
+    command1 = 2;
+    isMuted1 = false;
+    display_cycleOne = false;
+    if (!display_cycleTwo)
+    {
+      bLcdSetup = false;
+      display_cycleTwo = true;
+    }
+  }
+
+  /*
+      FAN N2 ( pin A5)
+  */
+  if (!(powerCheck(voltage2, VOLTAGE_LVL))) // if u need to mute track n1
+  {
+    command2 = 0;
+  }
+  if (powerCheck(voltage2, VOLTAGE_LVL) ) // if u need to unmute track 1
+  {
+    command2 = 2;
+  }
+
+  /*
+      FAN N3 ( pin A7)
+  */
+  if (!(powerCheck(voltage3, VOLTAGE_LVL))) // if u need to mute track n1
+  {
+    command3 = 0;
+  }
+  if (powerCheck(voltage3, VOLTAGE_LVL) ) // if u need to unmute track 1
+  {
+    command3 = 2;
+  }
+  /*
+        FAN N4 ( pin A9)
+  */
+  if (!(powerCheck(voltage4, VOLTAGE_LVL))) // if u need to mute track n1
+  {
+    command4 = 0;
+  }
+  if (powerCheck(voltage4, VOLTAGE_LVL) ) // if u need to unmute track 1
+  {
+    command4 = 2;
+  }
+
+}
+
+// SUPPORT FUNCTIONS
+bool powerCheck (float vol, float level) // comparing read voltage to barrier one
+{
+  if ( vol < level)
+  {
+    signalRec = false;
+  }
+  else
+  {
+    signalRec = true;
+  }
+  return signalRec;
+}
+
+void songDown()
+{
+  if (trackNum > 1)
+  {
+    trackNum--;
+  }
+  else
+  {
+    trackNum = MAX_SONGS;
+  }
+}
+
+void songUp()
+{
+  if (trackNum < MAX_SONGS)
+  {
+    trackNum++;
+  }
+  else
+  {
+    trackNum = 1;
+  }
+}
+void display_update()
+{
+  trackVol = trackVol + lastVol;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Song 0");
+  lcd.print(trackNum);
+  if (!isPaused) {
+    lcd.print(" playing");
+  }
+  else
+  {
+    lcd.print(" paused");
+  }
+
+  lcd.setCursor(0, 1);             // Установка курсора в начало второй строки
+  lcd.print("Volume: 0");
+  lcd.print(trackVol);
+
+  bLcdSetup = true;
+  trackVol = 0;
+}
